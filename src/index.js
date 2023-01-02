3b37d1bfe6402dcd0e9603dffffa4c4b2db8b0ad
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-01-02 17:05:28 +0000

    :hammer: added routing logic for S3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserHistory } from 'history';
+import { render } from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <App />
-);
+const history = createBrowserHistory();
+
+// S3 routing solution taken from: https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3
+if (root) {
+	const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+	if (path) {
+		history.replace(path);
+	}
+
+	root.render(<App />);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
